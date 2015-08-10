0ba2f7c294462f4c3a093dd57fee1649a93338e6
Olof Andersson <olof.andersson@gmail.com>
2015-08-10 23:41:00 +0200

    stub libk

---
+++ libk/include/libk.h
@@ -0,0 +1,14 @@
+#ifndef _LIBK_H
+#define _LIBK_H 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void libk_test();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ libk/libk.cpp
@@ -0,0 +1,8 @@
+#include <libk.h>
+#include <stdio.h>
+
+void libk_test()
+{
+	printf("Libk test\n");
+}
+
