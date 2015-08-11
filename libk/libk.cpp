51e62ae4c769e135384551692c87173709662fef
Olof Andersson <olof.andersson@gmail.com>
2015-08-11 22:48:51 +0200

    added python lib

--- libk/libk.cpp
+++ libk/libk.cpp
@@ -1,8 +1,49 @@
-#include <libk.h>
 #include <stdio.h>
+#include <sys/unistd.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-void libk_test()
+
+unsigned sleep(unsigned seconds)
 {
-	printf("Libk test\n");
+	return seconds;
 }
 
+int fcntl(int fd, int cmd, ...)
+{
+	return 0;
+}
+
+
+int truncate(const char *path, off_t length)
+{
+	return -1;
+}
+
+int ftruncate(int fd, off_t length)
+{
+	return -1;
+}
+
+void flockfile(FILE *filehandle)
+{
+	return;
+}
+
+//int ftrylockfile(FILE *filehandle);
+
+void funlockfile(FILE *filehandle)
+{
+	return;
+}
+
+FILE *popen(const char *command, const char *type)
+{
+	return NULL;
+}
+
+int pclose(FILE *stream)
+{
+	return -1;
+}
