aef26326863a005819b2a2677bf069573aa2d3ea
Olof Andersson <olof.andersson@gmail.com>
2015-08-23 14:49:32 +0200

    removed explicit conversions from printf statements

--- kernel/memory/multiboot_mmap.cpp
+++ kernel/memory/multiboot_mmap.cpp
@@ -11,24 +11,22 @@ void multiboot_mmap(unsigned long magic, multiboot_info_t *mbi)
 	/* Am I booted by a Multiboot-compliant boot loader? */
 	if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
 	{
-		printf("Invalid magic number: 0x%x\n", (unsigned) magic);
+		printf("Invalid magic number: 0x%lx\n", magic);
 		return;
 	}
 
-	/* Print out the flags. */
-	printf("flags = 0x%x\n", (unsigned) mbi->flags);
+	printf("flags = 0x%lx\n", mbi->flags);
 
 	/* Are mem_* valid? */
 	if (CHECK_FLAG(mbi->flags, 0))
 	{
-		printf("mem_lower = %uKB, mem_upper = %uKB\n",
-				(unsigned) mbi->mem_lower, (unsigned) mbi->mem_upper);
+		printf("mem_lower = %luKiB, mem_upper = %luKiB\n", mbi->mem_lower, mbi->mem_upper);
 	}
 
 	/* Is boot_device valid? */
 	if (CHECK_FLAG(mbi->flags, 1))
 	{
-		printf("boot_device = 0x%x\n", (unsigned) mbi->boot_device);
+		printf("boot_device = 0x%lx\n", mbi->boot_device);
 	}
 
 	/* Is the command line passed? */
@@ -42,13 +40,12 @@ void multiboot_mmap(unsigned long magic, multiboot_info_t *mbi)
 	{
 		multiboot_module_t *mod;
 		unsigned int i;
-
-		printf("mods_count = %d, mods_addr = 0x%x\n", (int) mbi->mods_count, (int) mbi->mods_addr);
-		for (i = 0, mod = (multiboot_module_t *) mbi->mods_addr;
-				i < mbi->mods_count; i++, mod++)
+		printf("mods_count = %ld, mods_addr = 0x%lx\n", mbi->mods_count, mbi->mods_addr);
+		for (i = 0, mod = (multiboot_module_t *) mbi->mods_addr; i < mbi->mods_count; i++, mod++)
 		{
-			printf(" mod_start = 0x%x, mod_end = 0x%x, cmdline = %s\n",
-					(unsigned) mod->mod_start, (unsigned) mod->mod_end,
+			printf(" mod_start = 0x%lx, mod_end = 0x%lx, cmdline = %s\n",
+					mod->mod_start,
+					mod->mod_end,
 					(char *) mod->cmdline);
 		}
 	}
@@ -65,33 +62,31 @@ void multiboot_mmap(unsigned long magic, multiboot_info_t *mbi)
 	{
 		multiboot_aout_symbol_table_t *multiboot_aout_sym = &(mbi->u.aout_sym);
 
-		printf("multiboot_aout_symbol_table: tabsize = 0x%0x, "
-				"strsize = 0x%x, addr = 0x%x\n",
-				(unsigned) multiboot_aout_sym->tabsize,
-				(unsigned) multiboot_aout_sym->strsize,
-				(unsigned) multiboot_aout_sym->addr);
+		printf("multiboot_aout_symbol_table: tabsize = 0x%0lx, "
+				"strsize = 0x%lx, addr = 0x%lx\n",
+				multiboot_aout_sym->tabsize,
+				multiboot_aout_sym->strsize,
+				multiboot_aout_sym->addr);
 	}
 
 	/* Is the section header table of ELF valid? */
 	if (CHECK_FLAG(mbi->flags, 5))
 	{
-		multiboot_elf_section_header_table_t *multiboot_elf_sec =
-				&(mbi->u.elf_sec);
-
-		printf("multiboot_elf_sec: num = %u, size = 0x%x,"
-				" addr = 0x%x, shndx = 0x%x\n",
-				(unsigned) multiboot_elf_sec->num,
-				(unsigned) multiboot_elf_sec->size,
-				(unsigned) multiboot_elf_sec->addr,
-				(unsigned) multiboot_elf_sec->shndx);
+		multiboot_elf_section_header_table_t *multiboot_elf_sec = &(mbi->u.elf_sec);
+		printf("multiboot_elf_sec: num = %lu, size = 0x%lx,"
+				" addr = 0x%lx, shndx = 0x%lx\n",
+				multiboot_elf_sec->num,
+				multiboot_elf_sec->size,
+				multiboot_elf_sec->addr,
+				multiboot_elf_sec->shndx);
 	}
 
 	/* Are mmap_* valid? */
 	if (CHECK_FLAG(mbi->flags, 6))
 	{
 		multiboot_memory_map_t *mmap;
-
-		printf("mmap_addr = 0x%lx, mmap_length = 0x%lx\n", mbi->mmap_addr,
+		printf("mmap_addr = 0x%lx, mmap_length = 0x%lx\n",
+				mbi->mmap_addr,
 				mbi->mmap_length);
 
 		mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
@@ -102,8 +97,7 @@ void multiboot_mmap(unsigned long magic, multiboot_info_t *mbi)
 			printf("length = %8llx, ", mmap->len);
 			printf("type = %lx", mmap->type);
 			printf("\n");
-			mmap = (multiboot_memory_map_t *) ((unsigned long) mmap + mmap->size
-					+ sizeof(mmap->size));
+			mmap = (multiboot_memory_map_t *) ((unsigned long) mmap + mmap->size + sizeof(mmap->size));
 		}
 	}
 }
