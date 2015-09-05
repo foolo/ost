e23ee4501be520e4e2437f8fb5ff55bed6b0e9e6
Olof Andersson <olof.andersson@gmail.com>
2015-09-05 20:02:42 +0200

    changed eclipse debugging settings to run .bin output

--- .settings/launch_qemu_debugger.sh
+++ .settings/launch_qemu_debugger.sh
@@ -2,7 +2,7 @@
 
 # Eclipse calls gdb twice. First time with --version
 if [[ $2 != --version ]]; then
-  qemu-system-i386 -gdb tcp::1234 -S -cdrom $1 &
+  qemu-system-i386 -gdb tcp::1234 -S -kernel $1 &
 fi
 
 # Shift and pass parameters to gdb
