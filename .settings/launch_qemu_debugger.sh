0540eef1d5ea176415a2c3fa0825306178c14e10
Olof Andersson <olof.andersson@gmail.com>
2016-05-19 20:19:30 +0200

    added EXTRA_COMMAND parameter to launch_qemu_debugger.sh using it in Qemu Debug configuration

--- .settings/launch_qemu_debugger.sh
+++ .settings/launch_qemu_debugger.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Eclipse calls gdb twice. First time with --version
-if [[ $2 != --version ]]; then
-  qemu-system-i386 -gdb tcp::1234 -S -kernel $1 &
+# Usage:   launch_bash_debugger.sh  EXTRA_COMMAND  GDB_ARGS  [--version]
+# Example: launch_bash_debugger.sh: "qemu-system-i386 -gdb tcp::1234 -S -kernel kernel.bin" <GDB_ARGS>
+
+for LAST_ARG; do true; done  #get last argument
+
+EXTRA_COMMAND=$1
+
+# Eclipse calls gdb twice. First time with --version.
+if [[ $LAST_ARG != --version ]]; then
+	bash -c "$EXTRA_COMMAND &"
 fi
 
 # Shift and pass parameters to gdb
