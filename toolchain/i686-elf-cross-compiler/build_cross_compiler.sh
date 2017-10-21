0fe63fd6320c1188340da2617bfd74b0e7380da7
Olof Andersson <olof.andersson@gmail.com>
2017-10-21 21:46:11 +0200

    fixed download directory path in build_cross_compiler.sh

--- toolchain/i686-elf-cross-compiler/build_cross_compiler.sh
+++ toolchain/i686-elf-cross-compiler/build_cross_compiler.sh
@@ -10,13 +10,13 @@ cd external
 
 rm -rf *
 
-tar xf $DOWNLOADS/binutils-2.24.tar.gz
-tar xf $DOWNLOADS/gcc-4.9.2.tar.gz
-tar xf $DOWNLOADS/mpfr-3.1.2.tar.xz
-tar xf $DOWNLOADS/gmp-6.0.0a.tar.xz
-tar xf $DOWNLOADS/mpc-1.0.2.tar.gz
-tar xf $DOWNLOADS/isl-0.12.2.tar.bz2
-tar xf $DOWNLOADS/cloog-0.18.1.tar.gz
+tar xf ../../downloads/binutils-2.24.tar.gz
+tar xf ../../downloads/gcc-4.9.2.tar.gz
+tar xf ../../downloads/mpfr-3.1.2.tar.xz
+tar xf ../../downloads/gmp-6.0.0a.tar.xz
+tar xf ../../downloads/mpc-1.0.2.tar.gz
+tar xf ../../downloads/isl-0.12.2.tar.bz2
+tar xf ../../downloads/cloog-0.18.1.tar.gz
 
 cd gcc-4.9.2
 ln -s ../mpfr-3.1.2 mpfr
